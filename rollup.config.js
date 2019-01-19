e6c54ea54c3b243e4e6f2d0915560c2234620043
Niek Saarberg <n.saarberg@gmail.com>
2019-01-19 14:37:47 +0100

    Removed newline

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs'
 
-
 import pkg from './package.json';
 
 const banner = `/**
